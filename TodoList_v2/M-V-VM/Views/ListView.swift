797125b874af8ebe6c67817e5ca04180819e0b78
Jack <jwz1101@hotmail.com>
2025-03-23 21:15:45 +0000

    [Feature] Video 2 - Add variable / Edit Button / Add Button

--- TodoList_v2/M-V-VM/Views/ListView.swift
+++ TodoList_v2/M-V-VM/Views/ListView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct ListView: View {
     
+    @State var items: [String] = [
+        "This is the first item!",
+        "This is the second item!",
+        "This is the third item!",
+    ]
+    
+    
     var body: some View {
         List {
-            ListRowView(title: "First Title")
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
         }
         .navigationTitle("Todo List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing: NavigationLink("Add", destination: Text("Destination"))
+        )
         
     }
 }
