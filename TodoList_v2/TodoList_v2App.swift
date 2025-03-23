b333325019f502f279de2235b5943817fef446b6
Jack <jwz1101@hotmail.com>
2025-03-23 20:17:34 +0000

    Initial Commit

---
+++ TodoList_v2/TodoList_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_v2App.swift
+//  TodoList_v2
+//
+//  Created by Jack Lee on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
