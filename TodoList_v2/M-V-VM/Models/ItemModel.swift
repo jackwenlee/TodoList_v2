0c939ffd7e75ba16c7866f8d4763c04c12fbfc63
Jack <jwz1101@hotmail.com>
2025-03-30 13:29:38 +0100

    [Feature] Video 4 - Tapping the screen will toggle between completed and not completed.

--- TodoList_v2/M-V-VM/Models/ItemModel.swift
+++ TodoList_v2/M-V-VM/Models/ItemModel.swift
@@ -8,7 +8,21 @@
 import Foundation
 
 struct ItemModel: Identifiable {
-    let id = UUID()
+    let id: String
     let title: String
     let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+    
 }
+
+
